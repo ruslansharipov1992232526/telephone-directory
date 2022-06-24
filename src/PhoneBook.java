b96b5d0b33c6e97a1c3be772c496c8d98324a9b3
Ruslan Sharipov <ruslansharipov1992@yandex.ru>
2022-06-24 15:58:57 +0500

    Добавиk метод HashCode

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,13 +1,18 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 public class PhoneBook extends Contact {
     public PhoneBook(String name, int phone) {
         super(name, phone);
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
     public static void a() {
-        HashMap<String, Integer> phones = new HashMap<>();
+        HashMap<String, List<Contact>> phones = new HashMap<>();
         phones.put("Ruslan", 22);
         phones.put("tol", 22);
         phones.put("Russalan", 22);
